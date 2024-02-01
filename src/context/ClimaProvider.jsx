a3ff3767fb806c7433bb9473e0f26d53f81bef22
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-02-01 20:24:14 -0300

    consultando la API de clima

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -1,4 +1,7 @@
+/* eslint-disable react/prop-types */
+// import { axios } from "axios"
 import { createContext, useState } from "react"
+
 
 
 export const ClimaContext = createContext()
@@ -6,6 +9,8 @@ export const ClimaContext = createContext()
 
 
 const ClimaProvider = ({ children }) => {
+
+    console.log(import.meta.env.VITE_API_KEY)
 
     const [busqueda, setBusqueda] = useState(
         {
@@ -22,8 +27,34 @@ const ClimaProvider = ({ children }) => {
     }
 
 
-    const consultarClima = (datos) => {
-        console.log(datos)
+    const consultarClima = async (datos) => {
+        try {
+            const { ciudad, pais } = datos
+
+
+            const keyId = import.meta.env.VITE_API_KEY
+
+            const url = (`http://api.openweathermap.org/data/2.5/forecast?q=${ciudad},${pais}&appid=${keyId}`)
+
+            const resAPI = await fetch(url)
+            const resultado = await resAPI.json()
+
+            console.log(resultado.city)
+            console.log(resultado.city.coord)
+
+            // const { lat, lon } = url[0]
+
+            // const urlClima = (`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${keyId}`)
+
+            // const resAPIClima = await fetch(urlClima)
+            // const resultadoClima = await resAPIClima.json()
+
+            // console.log(resultadoClima)
+
+
+        } catch (error) {
+            console.log(error)
+        }
     }
 
 
